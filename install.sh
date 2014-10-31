5cf8289fd678699acaa658215949754edff73d04
xbgmsharp <xbgmsharp@gmail.com>
2014-10-31 13:34:54 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-# upgrade system
-apt-get update
-apt-get dist-upgrade
+# Upgrade system
+apt-get update & apt-get -y dist-upgrade
 
-# Install basic complication tools
-apt-get install make gcc
+# Install basic complication tools and dev libraries
+apt-get -y install make gcc zlib1g-dev libc6-dev libssl-dev libstdc++6-4.7-dev libc-dev-bin
+
+# Install CGI perl dependencies
+apt-get -y install liburi-perl libfcgi-perl libconfig-inifiles-perl libipc-system-simple-perl libsub-override-perl
 
 #  Prepare iPXE directory
-apt-get install liburi-perl libfcgi-perl libconfig-inifiles-perl libipc-system-simple-perl libsub-override-perl
 mkdir -p /var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build
 rm -rf /var/cache/ipxe-build/* /var/run/ipxe-build/* /var/tmp/ipxe-build/*
 
+# Install Git tools
+apt-get -y install git-core
+
 # Prepare the git iPXE repository
-apt-get install git-core
 cd /var/tmp/ && rm -rf ipxe && git clone https://git.ipxe.org/ipxe.git
 touch /var/run/ipxe-build/ipxe-build-cache.lock
 chown -R www-data:www-data /var/run/ipxe-build/ipxe-build-cache.lock /var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build /var/tmp/ipxe
 
-# Apache 2 wtih fast CGI module
-apt-get install libapache2-mod-fcgid && a2enmod fcgid
+# Install Apache 2 with fast CGI and PHP5 module
+apt-get -y install libapache2-mod-fcgid libapache2-mod-php5 && a2enmod fcgid php5
 
-# JSON library Perl
-apt-get install libjson-perl libjson-any-perl libjson-xs-perl
+# Install JSON library Perl
+apt-get -y install libjson-perl libjson-any-perl libjson-xs-perl
 
-# Allow to build ISO and EFI binary
-apt-get install binutils-dev genisoimage
+# Install to allow to build ISO and EFI binary
+apt-get -y install binutils-dev genisoimage
 
 # Prepare the git buildweb repository
 mkdir -p /var/www/ipxe-buildweb
 git clone https://github.com/xbgmsharp/ipxe-buildweb.git
-
 cp parseheaders.pl /var/tmp/ipxe/src/util/
 
+# message
+echo -e "You can now configure your webserver Apache.\n"
+echo -e "Important directory:\n"
+echo -e "/var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build /var/www/ipxe-buildweb\n"
