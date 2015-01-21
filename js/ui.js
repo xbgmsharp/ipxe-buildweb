c694f0484f0e563fddb6c1e63f715e296e175fa9
xbgmsharp <xbgmsharp@gmail.com>
2015-01-21 23:58:35 +0900

    Update Copyright for 2015

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@
  * ================================================================================
  * Dynamic iPXE image generator
  *
- * Copyright (C) 2012-2014 Francois Lacroix. All Rights Reserved.
+ * Copyright (C) 2012-2015 Francois Lacroix. All Rights Reserved.
  * Website: http://ipxe.org, https://github.com/xbgmsharp/ipxe-buildweb
  * License: GNU General Public License version 3 or later; see LICENSE.txt
  * ================================================================================
