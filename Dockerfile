4bee4586be8b31699c43f2ea717df074f8dc99a6
xbgmsharp <flacroix>
2021-11-25 15:00:04 +0100

    Update Docker to latest Ubuntu LTS

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 #------------------------------------------------------------------------
 # Dynamic iPXE image generator
 #
-# Copyright (C) 2012-2019 Francois Lacroix. All Rights Reserved.
+# Copyright (C) 2012-2021 Francois Lacroix. All Rights Reserved.
 # License:  GNU General Public License version 3 or later; see LICENSE.txt
 # Website:  http://ipxe.org, https://github.com/xbgmsharp/ipxe-buildweb
 #------------------------------------------------------------------------
 #
-# Ubuntu LTS 14.04 + Apache2 + module + my app
+# Ubuntu LTS + Apache2 + module + my app
 #
 # Base from ultimate-seed Dockerfile
 # https://github.com/pilwon/ultimate-seed
@@ -18,25 +18,28 @@
 # VERSION 0.0.1
 
 # Pull base image.
-FROM ubuntu:14.04
+FROM ubuntu:latest
 MAINTAINER Francois Lacroix <xbgmsharp@gmail.com>
 
 # Setup system and install tools
-RUN echo "initscripts hold" | dpkg --set-selections
+#RUN echo "initscripts hold" | dpkg --set-selections
+RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 RUN echo 'alias ll="ls -lah --color=auto"' >> /etc/bash.bashrc
+
+# Make sure the package repository is up to date
+RUN apt-get update && apt-get -yq upgrade
 
 # Set locale
 RUN apt-get -qqy install locales
-RUN locale-gen --purge en_US en_US.UTF-8
-RUN dpkg-reconfigure locales
+#RUN locale-gen --purge en_US en_US.UTF-8
+#RUN dpkg-reconfigure locales
+RUN localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+ENV LANG en_US.utf8
 ENV LC_ALL en_US.UTF-8
 
 # Set ENV
 ENV HOME /root
 ENV DEBIAN_FRONTEND noninteractive
-
-# Make sure the package repository is up to date
-RUN apt-get update && apt-get -y upgrade
 
 # Install SSH
 RUN apt-get install -y openssh-server
@@ -73,12 +76,12 @@ EXPOSE 22 80
 RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Make sure the package repository is up to date
-ONBUILD apt-get update && apt-get -y upgrade
+ONBUILD apt-get update && apt-get -yq upgrade
 ONBUILD apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 RUN chmod +x /var/www/ipxe-buildweb/install.sh
 RUN chmod +x /etc/start.sh
 
-RUN /etc/init.d/apache2 start
+#RUN /etc/init.d/apache2 start
 #ENTRYPOINT ["/usr/bin/tail","-f","/var/log/apache2/access.log"]
 ENTRYPOINT ["/etc/start.sh"]
--- install.sh
+++ install.sh
@@ -2,40 +2,55 @@
 #------------------------------------------------------------------------
 # Dynamic iPXE image generator
 #
-# Copyright (C) 2012-2019 Francois Lacroix. All Rights Reserved.
+# Copyright (C) 2012-2021 Francois Lacroix. All Rights Reserved.
 # License:  GNU General Public License version 3 or later; see LICENSE.txt
 # Website:  http://ipxe.org, https://github.com/xbgmsharp/ipxe-buildweb
 #------------------------------------------------------------------------
 
+# Fix "Error debconf: unable to initialize frontend: Dialog"
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 # Upgrade system
-apt-get update && apt-get -y dist-upgrade
+apt-get update && apt-get -yq dist-upgrade
 
 # Install basic compilation tools and dev libraries
-apt-get -y install make gcc zlib1g-dev libc6-dev libssl-dev libstdc++6-4.7-dev libc-dev-bin liblzma-dev
+apt install -yq build-essential \
+                    iasl lzma-dev mtools perl python \
+                    subversion uuid-dev liblzma-dev mtools
+#apt-get -y install make gcc zlib1g-dev libc6-dev libssl-dev libstdc++6-4.7-dev libc-dev-bin liblzma-dev
 
 # Install CGI Perl dependencies
-apt-get -y install liburi-perl libfcgi-perl libconfig-inifiles-perl libipc-system-simple-perl libsub-override-perl
+apt-get -yq install liburi-perl \
+                      libfcgi-perl \
+                      libconfig-inifiles-perl \
+                      libipc-system-simple-perl \
+                      libsub-override-perl \
+                      libcgi-pm-perl
 
 #  Prepare iPXE directory
 mkdir -p /var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build
 rm -rf /var/cache/ipxe-build/* /var/run/ipxe-build/* /var/tmp/ipxe-build/*
 
 # Install Git tools
-apt-get -y install git-core
+apt-get -y install git
 
 # Prepare the git iPXE repository
-cd /var/tmp/ && rm -rf ipxe && git clone https://git.ipxe.org/ipxe.git
+cd /var/tmp/ && rm -rf ipxe && git clone https://github.com/ipxe/ipxe/
 touch /var/run/ipxe-build/ipxe-build-cache.lock
-chown -R www-data:www-data /var/run/ipxe-build/ipxe-build-cache.lock /var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build /var/tmp/ipxe
+chown -R www-data:www-data /var/run/ipxe-build/ipxe-build-cache.lock \
+                           /var/cache/ipxe-build \
+                           /var/run/ipxe-build \
+                           /var/tmp/ipxe-build /var/tmp/ipxe
 
-# Install Apache 2 with fast CGI and PHP5 module
-apt-get -y install libapache2-mod-fcgid libapache2-mod-php5 && a2enmod fcgid php5
+# Install Apache with fast CGI and PHP module
+#apt-get -y install libapache2-mod-fcgid libapache2-mod-php5 && a2enmod fcgid php5
+apt-get -yq install libapache2-mod-fcgid libapache2-mod-php && a2enmod fcgid php7.4
 
 # Install JSON library Perl
-apt-get -y install libjson-perl libjson-any-perl libjson-xs-perl
+apt-get -yq install libjson-perl libjson-any-perl libjson-xs-perl
 
 # Install extra packages to allow to build ISO and EFI binary
-apt-get -y install binutils-dev genisoimage syslinux
+apt-get -yq install binutils-dev genisoimage syslinux isolinux
 
 # Prepare the git buildweb repository
 mkdir -p /var/www && cd /var/www && git clone https://github.com/xbgmsharp/ipxe-buildweb.git
@@ -73,3 +88,12 @@ cat << EOF > /etc/apache2/mods-enabled/fcgid.conf
     </Files>
 </IfModule>
 EOF
+
+DIRECTORIES="/var/tmp/ipxe-build /var/tmp/ipxe"
+for DIRECTORY in ${DIRECTORIES}
+do
+ if [[ ! -d "${DIRECTORY}" ]]; then
+     echo "Error, missing directory ${DIRECTORY}"
+     exit 1
+ fi
+done
